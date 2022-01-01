403f9c8ec85d61352e657d953e91bd1cf0d676e8
Juanito Emia <Juanito Emia>
2022-01-01 23:18:36 +0800

    Add console.log to myApp.js

---
+++ myApp.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var bGround = require('fcc-express-bground');
+
+bGround.log("Hello World");
+console.log("hello world");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
